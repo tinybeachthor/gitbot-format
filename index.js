2b2a77c9f22cdf9565dbb8c765a17b9a1eb70ed0
Martin Toman <whomenope@outlook.com>
2019-09-27 12:44:44 +0200

    Get PR changed files contents

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const util = require('util')
+
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
@@ -9,14 +11,11 @@ module.exports = bot => {
   bot.on("pull_request.synchronize", check)
 
   async function check (context) {
-    const pull = context.issue()
+    const { owner, repo, number } = context.issue()
 		const { sha } = context.payload.pull_request.head
-		const repo = context.repo()
-
-    bot.log(`sha: ${sha}`)
 
     // GH API
-    const { checks } = context.github;
+    const { checks, pulls, git } = context.github
 
 		// Set PR check status
     const statusInfo = context.repo({
@@ -28,6 +27,27 @@ module.exports = bot => {
     await checks.create({
       ...statusInfo,
       status: "queued",
+    })
+
+    // Get changed files
+    const files = await pulls.listFiles({
+      owner,
+      repo,
+      pull_number: number,
+    })
+
+    files.data.forEach(({ filename, sha }) => {
+      bot.log(`\t${filename}`)
+      git.getBlob({
+        owner,
+        repo,
+        file_sha: sha,
+      }).then(({ data }) => {
+        bot.log(util.inspect(data))
+        const buffer = Buffer.from(data.content, 'base64')
+        const text = buffer.toString('ascii')
+        bot.log(text)
+      })
     })
 
     // Run formatter
