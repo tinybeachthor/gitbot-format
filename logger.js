a316a21f84b72ba9b06cf3f3a05ad6d76892eb85
Martin Toman <whomenope@outlook.com>
2019-09-28 12:54:51 +0200

    Fix: only show program name if set

--- logger.js
+++ logger.js
@@ -2,7 +2,8 @@ const { createLogger, format, transports } = require('winston')
 const { combine, timestamp, printf } = format
 
 const logFormat = printf(info => {
-  return `${(process.env.PROGRAM_ALIAS + ':') || ''}${info.timestamp}:${info.level}: ${info.message}`
+  const name = process.env.PROGRAM_ALIAS ? process.env.PROGRAM_ALIAS + ':' : ''
+  return `${name}${info.timestamp}:${info.level}: ${info.message}`
 })
 
 const logger = createLogger({
