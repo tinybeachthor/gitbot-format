c5f733e570f54996afc8307084f5566a37088896
Martin Toman <whomenope@outlook.com>
2019-09-28 11:43:58 +0200

    Check file extensions for valid c-like files

--- formatter.js
+++ formatter.js
@@ -1,16 +1,26 @@
+const path = require('path')
+
 module.exports = files => {
+
+  const extensions =
+    ['.c', '.h', '.cpp', '.hpp', '.C', '.H', '.cc', '.hh', '.cxx', '.hxx']
 
   let formatted = files
 
   function format() {
-    formatted = files.map(({ filename, content }) => {
-      const transformed = content + "\n"
-      return {
-        filename,
-        content: transformed,
-        touched: true,
-      }
-    })
+    formatted = files
+      .filter(({ filename }) => {
+        return extensions.includes(path.extname(filename))
+      })
+      .map(({ filename, content }) => {
+        const transformed = content + "\n"
+
+        return {
+          filename,
+          content: transformed,
+          touched: true,
+        }
+      })
   }
 
   function touched() {
