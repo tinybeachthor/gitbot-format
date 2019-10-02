236668a6aba9252464e564977fbb570e4e4afb53
Martin Toman <whomenope@outlook.com>
2019-10-02 12:11:00 +0200

    Use default branch stylefile before PR stylefile. Update README

--- README.md
+++ README.md
@@ -5,13 +5,22 @@ A GitHub App using the [Probot](https://github.com/probot/probot) framework
 Keeps your repos tidy by running ```clang-format``` on every PR.
 
 ## Features
-
-### Zero configuration
-No configuration neccessary, just install it and enjoy.
-
+### Respects repo stylefile
+Just a regular ```.clang-format``` file in the root of the repo.
+Looks on the default branch first, PR branch second, and lastly defaults
+to ```-style=Google```.
+### Zero setup
+No setup neccessary, just install in GitHub and enjoy.
 ### Fully stateless
 On every restart it rechecks all open PRs in installed repos.
 Making it easy to restart/upgrade/migrate/manage the deployment.
+
+## FAQ
+### Why prefer stylefile from the default branch to feature branch stylefile.
+> Since the goal is to keep the format of the whole codebase consistent,
+> prefering the default branch stylefile will enforce the same style on all
+> feature branches and make changes to the stylefile propagate instantly
+> (no need to rebase every feature branch)
 
 ## License
 [ISC](LICENSE) © 2019 Martin Toman <whomenope@outlook.com>
--- format.js
+++ format.js
@@ -9,12 +9,12 @@ const formatter = require('./formatter')
 const stylefileName = '.clang-format'
 
 async function getStylefile({owner, repo, ref}, repos, info) {
+  // try getting stylefile from default branch (master) first
   try {
     const stylefileResponse = await repos.getContents({
       owner,
       repo,
       path: stylefileName,
-      ref,
     })
     const buffer = Buffer.from(stylefileResponse.data.content, 'base64')
     const text = buffer.toString('utf8')
@@ -25,12 +25,36 @@ async function getStylefile({owner, repo, ref}, repos, info) {
       return acc
     }), {})
 
-    info(`Got stylefile : ${json}`)
+    info(`Got stylefile from default branch : ${json}`)
     return json
   }
+  // try getting stylefile from current branch
   catch (e) {
-    info('Could not get stylefile, falling back to defaults.')
-    return null
+    info('Could not get stylefile from default branch, trying PR branch.')
+    try {
+      const stylefileResponse = await repos.getContents({
+        owner,
+        repo,
+        path: stylefileName,
+        ref,
+      })
+      const buffer = Buffer.from(stylefileResponse.data.content, 'base64')
+      const text = buffer.toString('utf8')
+
+      // flatten YAML docs into single and convert YAML to JSON
+      const json = JSON.stringify(yaml.safeLoadAll(text).reduce((acc, doc) => {
+        Object.keys(doc).forEach((key) => acc[key] = doc[key])
+        return acc
+      }), {})
+
+      info(`Got stylefile from PR branch : ${json}`)
+      return json
+    }
+    // no stylefile anywhere, use default styling
+    catch (e) {
+      info('Could not get stylefile, falling back to defaults.')
+      return null
+    }
   }
 }
 
