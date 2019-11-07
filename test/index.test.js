7fbc009e0693e16633f6b7749c573ca6366f4521
Martin Toman <whomenope@outlook.com>
2019-11-07 16:06:59 +0100

    Test structure with fixture

--- test/index.test.js
+++ test/index.test.js
@@ -3,30 +3,35 @@ const nock = require('nock')
 const myProbotApp = require('..')
 const { Probot, createProbot } = require('probot')
 
+const payload = require('./fixtures/pull_request.opened.json')
+
 nock.disableNetConnect()
 
 describe('gitbot-format', () => {
+  process.env.NO_RERUN_ON_RESTART = 'true'
+
   let probot
   beforeEach(() => {
     probot = createProbot({ id: 1, cert: 'test', githubToken: 'test' })
     probot.load(myProbotApp)
+
   })
 
-  test('creates a passing check', async () => {
+  test('Format an opened PR', async () => {
+    nock('https://api.github.com')
+      .log(console.log)
+
     // Test that we correctly return a test token
     nock('https://api.github.com')
       .post('/app/installations/2/access_tokens')
       .reply(200, { token: 'test' })
 
-    // // Test that a comment is posted
-    // nock('https://api.github.com')
-    //   .post('/repos/hiimbex/testing-things/issues/1/comments', (body) => {
-    //     expect(body).toMatchObject(issueCreatedBody)
-    //     return true
-    //   })
-    //   .reply(200)
+    // 
+    nock('https://api.github.com')
+      .post('/repos/org-name/check-runs')
+      .reply()
 
-    // // Receive a webhook event
-    // await probot.receive({ name: 'issues', payload })
+    // Invoke bot
+    await probot.receive({name: 'pull_request', payload})
   })
 })
