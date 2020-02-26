ae2aa842abf18bb3818b1f3d862febfdf6e049f2
Martin Toman <whomenope@outlook.com>
2020-02-26 21:34:55 +0100

    Add tests for gitmodulesParser

---
+++ __tests__/gitmodulesParser.js
@@ -0,0 +1,25 @@
+const parser = require('gitmodulesParser')
+
+test('parses multiple submodules', () => {
+  const gitmodules = `
+  [submodule "a"]
+  \tpath = a
+  \turl = git@github.com:org/repoA.git
+  [submodule "dir1/dir2/b"]
+  \tpath = dir1/dir2/b
+	\turl = git@github.com:org/repoB.git
+  `
+  const expected = [
+    'a',
+    'dir1/dir2/b',
+  ]
+
+  expect(parser.parsePaths(gitmodules)).toBe(expected)
+})
+
+test('parses empty submodules', () => {
+  const gitmodules = ''
+  const expected = []
+
+  expect(parser.parsePaths(gitmodules)).toBe(expected)
+})
