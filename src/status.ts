0eaf07a32157fc130f340b70e571670bbb77dd8c
Martin Toman <whomenope@outlook.com>
2020-03-03 22:39:02 +0100

    Convert src/status.ts to typescript

--- src/status.ts
+++ src/status.ts
@@ -1,62 +1,58 @@
-module.exports = (checks, statusInfo) => {
+const successGifs = [
+  'https://imgur.com/xS6ZYWn.gif',
+  'https://imgur.com/xAIRmOG.gif',
+  'https://imgur.com/5B9jzkX.gif',
+  'https://imgur.com/CQoRHRT.gif',
+  'https://imgur.com/ZDdXOVR.gif',
+  'https://imgur.com/MGWv2GK.gif',
+  'https://imgur.com/XbFqF55.gif',
+  'https://imgur.com/vWpBm6U.gif',
+  'https://imgur.com/ymknBjY.gif',
+  'https://imgur.com/MSs0DLT.gif',
+  'https://imgur.com/JbrXIMG.gif',
+  'https://imgur.com/oJqCQbw.gif',
+
+  'https://i.imgur.com/6Ujoypl.jpg',
+  'https://i.imgur.com/kSb9HUf.gif',
+  'https://i.imgur.com/fS0rtRP.gif',
+  'https://i.imgur.com/XCNCgkF.gif',
+  'https://i.imgur.com/XmO1n7p.gif',
+  'https://i.imgur.com/2g2JcEA.gif',
+  'https://i.imgur.com/Q8YhMf8.gif',
+  'https://i.imgur.com/rprDnd7.gif',
+  'https://i.imgur.com/gfyNHob.gif',
+
+  'https://i0.kym-cdn.com/photos/images/original/001/151/241/bd9.gif',
+  'https://66.media.tumblr.com/1a877c907dd213474ca0061caa13fdee/tumblr_mwdenkWwv41qa94xto1_500.gif',
+  'https://i.imgur.com/6x7tbsD.gif',
+]
+const failureGifs = [
+  'https://media.giphy.com/media/gy4PFY2wH94RO/source.gif',
+  'https://i.imgur.com/OCdX290.gif',
+  'https://i.giphy.com/GaynE6dZoVDkQ.gif',
+  'https://i.imgur.com/VnTvaVc.gif',
+  'https://i.imgur.com/fhJMQ0x.gif',
+  'https://i.imgur.com/ZteXKIH.gif',
+  'https://i.imgur.com/xlxOxLG.gif',
+  'https://media.giphy.com/media/112CeAWuyhQX1C/source.gif',
+]
+
+function getForSha(collection: any[], hash: string) {
+  function hash2number(hash: string) {
+    return hash
+      .split('')
+      .map(c => c.charCodeAt(0))
+      .reduce((acc, x) => acc + x, 0)
+  }
+  return collection[hash2number(hash) % collection.length]
+}
+
+export default (checks, statusInfo) => {
 
   const sha = statusInfo.head_sha
-
-  function getForSha(hash) {
-    function hash2number(hash) {
-      return hash
-        .split('')
-        .map(c => c.charCodeAt(0))
-        .reduce((acc, x) => acc + x, 0)
-    }
-    return this[hash2number(hash) % this.length]
-  }
-
-  const successGifs = [
-    'https://imgur.com/xS6ZYWn.gif',
-    'https://imgur.com/xAIRmOG.gif',
-    'https://imgur.com/5B9jzkX.gif',
-    'https://imgur.com/CQoRHRT.gif',
-    'https://imgur.com/ZDdXOVR.gif',
-    'https://imgur.com/MGWv2GK.gif',
-    'https://imgur.com/XbFqF55.gif',
-    'https://imgur.com/vWpBm6U.gif',
-    'https://imgur.com/ymknBjY.gif',
-    'https://imgur.com/MSs0DLT.gif',
-    'https://imgur.com/JbrXIMG.gif',
-    'https://imgur.com/oJqCQbw.gif',
-
-    'https://i.imgur.com/6Ujoypl.jpg',
-    'https://i.imgur.com/kSb9HUf.gif',
-    'https://i.imgur.com/fS0rtRP.gif',
-    'https://i.imgur.com/XCNCgkF.gif',
-    'https://i.imgur.com/XmO1n7p.gif',
-    'https://i.imgur.com/2g2JcEA.gif',
-    'https://i.imgur.com/Q8YhMf8.gif',
-    'https://i.imgur.com/rprDnd7.gif',
-    'https://i.imgur.com/gfyNHob.gif',
-
-    'https://i0.kym-cdn.com/photos/images/original/001/151/241/bd9.gif',
-    'https://66.media.tumblr.com/1a877c907dd213474ca0061caa13fdee/tumblr_mwdenkWwv41qa94xto1_500.gif',
-    'https://i.imgur.com/6x7tbsD.gif',
-  ]
-  successGifs.getForSha = getForSha
-
-  const failureGifs = [
-		'https://media.giphy.com/media/gy4PFY2wH94RO/source.gif',
-    'https://i.imgur.com/OCdX290.gif',
-    'https://i.giphy.com/GaynE6dZoVDkQ.gif',
-    'https://i.imgur.com/VnTvaVc.gif',
-    'https://i.imgur.com/fhJMQ0x.gif',
-    'https://i.imgur.com/ZteXKIH.gif',
-    'https://i.imgur.com/xlxOxLG.gif',
-    'https://media.giphy.com/media/112CeAWuyhQX1C/source.gif',
-  ]
-  failureGifs.getForSha = getForSha
-
   let started_at = new Date()
 
-  function error (error) {
+  function error (error: string) {
     return checks.create({
       ...statusInfo,
       status: "completed",
@@ -81,7 +77,7 @@ module.exports = (checks, statusInfo) => {
     })
   }
 
-  function progress (time) {
+  function progress (time: Date) {
     started_at = time
 
     return checks.create({
@@ -95,7 +91,7 @@ module.exports = (checks, statusInfo) => {
     })
   }
 
-  function failure (annotations, lines, skipped) {
+  function failure (annotations: string[], lines: number, skipped: string[]) {
     const skipped_filenames = skipped.reduce((acc, filename) => `${acc}${filename};`, '')
     const skipped_report = skipped_filenames !== '' ? `\n\nFailed to get or process following files:\n${skipped_filenames}` : ''
     return checks.create({
@@ -110,7 +106,7 @@ module.exports = (checks, statusInfo) => {
         images: [
           {
             alt: 'sad',
-            image_url: failureGifs.getForSha(sha),
+            image_url: getForSha(failureGifs, sha),
           },
         ],
         annotations: annotations.slice(0, 50), // Limited to 50 at a time
@@ -125,7 +121,7 @@ module.exports = (checks, statusInfo) => {
     })
   }
 
-  function warningSkipped (skipped) {
+  function warningSkipped (skipped: string[]) {
     const options = {
       ...statusInfo,
       status: "completed",
@@ -169,7 +165,7 @@ module.exports = (checks, statusInfo) => {
         images: [
           {
             alt: 'happy',
-            image_url: successGifs.getForSha(sha),
+            image_url: getForSha(successGifs, sha),
           },
         ],
       },
@@ -181,6 +177,7 @@ module.exports = (checks, statusInfo) => {
 
     queued,
     progress,
+
     failure,
     warningSkipped,
     success,
