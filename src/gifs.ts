b76cab4f26b7cbe45bcf85462aa1a833e0d5a3e6
Martin Toman <whomenope@outlook.com>
2020-03-08 18:45:58 +0100

    Move gifs arrays to own module

---
+++ src/gifs.ts
@@ -0,0 +1,39 @@
+export const successGifs = [
+  'https://imgur.com/xS6ZYWn.gif',
+  'https://imgur.com/xAIRmOG.gif',
+  'https://imgur.com/5B9jzkX.gif',
+  'https://imgur.com/CQoRHRT.gif',
+  'https://imgur.com/ZDdXOVR.gif',
+  'https://imgur.com/MGWv2GK.gif',
+  'https://imgur.com/XbFqF55.gif',
+  'https://imgur.com/vWpBm6U.gif',
+  'https://imgur.com/ymknBjY.gif',
+  'https://imgur.com/MSs0DLT.gif',
+  'https://imgur.com/JbrXIMG.gif',
+  'https://imgur.com/oJqCQbw.gif',
+
+  'https://i.imgur.com/6Ujoypl.jpg',
+  'https://i.imgur.com/kSb9HUf.gif',
+  'https://i.imgur.com/fS0rtRP.gif',
+  'https://i.imgur.com/XCNCgkF.gif',
+  'https://i.imgur.com/XmO1n7p.gif',
+  'https://i.imgur.com/2g2JcEA.gif',
+  'https://i.imgur.com/Q8YhMf8.gif',
+  'https://i.imgur.com/rprDnd7.gif',
+  'https://i.imgur.com/gfyNHob.gif',
+
+  'https://i0.kym-cdn.com/photos/images/original/001/151/241/bd9.gif',
+  'https://66.media.tumblr.com/1a877c907dd213474ca0061caa13fdee/tumblr_mwdenkWwv41qa94xto1_500.gif',
+  'https://i.imgur.com/6x7tbsD.gif',
+]
+
+export const failureGifs = [
+  'https://media.giphy.com/media/gy4PFY2wH94RO/source.gif',
+  'https://i.imgur.com/OCdX290.gif',
+  'https://i.giphy.com/GaynE6dZoVDkQ.gif',
+  'https://i.imgur.com/VnTvaVc.gif',
+  'https://i.imgur.com/fhJMQ0x.gif',
+  'https://i.imgur.com/ZteXKIH.gif',
+  'https://i.imgur.com/xlxOxLG.gif',
+  'https://media.giphy.com/media/112CeAWuyhQX1C/source.gif',
+]
