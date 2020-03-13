33dcf42b386ce914a707b2c3501422240ba3fa45
Martin Toman <whomenope@outlook.com>
2020-03-13 01:43:47 +0100

    Fix index.js for action

---
+++ src/index.ts
@@ -0,0 +1,57 @@
+import * as core from '@actions/core'
+import {GitHub, context as _context} from '@actions/github'
+
+import Status from './status'
+import {lint, format} from './actions'
+
+async function run () {
+  // Get clang-format config path
+  const config_path = core.getInput('config-path')
+  console.log(`Using config at '${config_path}'`)
+
+  // Get authenticated API
+  const repoToken = core.getInput('repo-token')
+  const octokit = new GitHub(repoToken)
+
+  // Check permissions
+  const proactive = process.env.TAKE_ACTION ? process.env.TAKE_ACTION : false
+
+  // Get PR details
+  const context = _context
+  const {owner, repo} = context.repo
+  const {number: pull_number} = context.payload.issue || context.payload
+
+  const pr = await octokit.pulls.get({
+    owner,
+    repo,
+    pull_number,
+  })
+  const {sha, ref} = pr.data.head
+
+  // Setup PR status check
+  const status = Status(octokit.checks, {
+    owner,
+    repo,
+    name: 'clang-format',
+    head_sha: sha,
+  })
+  await status.queued()
+
+  // Run
+  try {
+    const details = { owner, repo, pull_number, sha, ref }
+
+    if (!proactive) {
+      await lint(details, octokit, status)
+    }
+    else {
+      await format(details, octokit, status)
+    }
+  }
+  catch (error) {
+    status.error(error)
+  }
+}
+
+// Run action
+run().catch((error) => core.setFailed(error.message))
